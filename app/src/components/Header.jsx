095baf8f0d5f066ab7fc51107fc3826c3b41e825
Rejoice <uahomorejoice@gmail.com>
2023-12-08 18:11:14 +0100

    logo

--- app/src/components/Header.jsx
+++ app/src/components/Header.jsx
@@ -1,7 +1,11 @@
+import logo from "./logo.png"
+
+
 export default function Header({ onNewChat }) {
     return (
-        <div className="flex flex-row p-4 bg-slate-500 rounded-xl my-4">
-            <p className="text-3xl text-slate-200 font-semibold grow">Ishop AI</p>
+        <div className="flex flex-row justify-between p-4 bg-slate-500 rounded-xl my-4">
+            <img style={{ width: "120px", height: "40px" }} src={logo} alt="" />
+            <p className="text-3xl text-slate-200 font-semibold">Ishop AI</p>
             <button
                 className="bg-slate-400 hover:bg-pink-400 text-white font-bold py-2 px-4 rounded"
                 onClick={onNewChat}
